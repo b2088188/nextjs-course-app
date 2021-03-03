46c299682477663f6078da5aff07cb0253787758
Shunze Lin <4A490101@stust.edu.tw>
2021-03-03 09:51:07 +0800

    feat: Getting the page param by using useRouter

--- pages/notes/[id].js
+++ pages/notes/[id].js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 
 const Page = () => {
-	return <div>Note Page</div>;
+	const {
+		query: { id }
+	} = useRouter();
+
+	return <div>Note Page {id}</div>;
 };
 
 export default Page;
