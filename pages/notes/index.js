18702cc919a663885854d1bae4f3f8e8f4e24545
Shunze Lin <4A490101@stust.edu.tw>
2021-03-03 11:34:51 +0800

    feat:Using Link component and router.push to navigate on client side

--- pages/notes/index.js
+++ pages/notes/index.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const Page = () => {
-	return <div>Note Index Page</div>;
+	const notes = new Array(15).fill(1).map((e, i) => ({ id: i, title: `Note: ${i}` }));
+	const router = useRouter();
+	return (
+		<div>
+			<h1>Notes</h1>
+
+			{notes.map((note) => (
+				<div>
+					{/*This is only for client side routing*/}
+					<Link key={note.id} href='/notes/[id]' as={`/notes/${note.id}`}>
+						<a>
+							<strong>{note.title}</strong>
+						</a>
+					</Link>
+					{/*This is only for client side routing*/}
+					<button onClick={() => router.push('/notes/[id]', `/notes/${note.id}`)}>
+						Programmatic routing to {note.tilte}
+					</button>
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default Page;
